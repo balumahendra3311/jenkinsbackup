3a7f54e4cba12f67063571d29d811dd3c5ed5c65
Ubuntu <ubuntu@ip-172-31-17-120.us-west-2.compute.internal>
2024-04-10 15:36:16 +0000

    added backup script

---
+++ jenkins_backup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# variables
+GIT_REPO="https://github.com/balumahendra3311/jenkinsbackup.git"
+LOCAL_DIR="/home/ubuntu/jenkins/backup/jenkinsbackup"
+JENKINS_HOME="/var/lib/jenkins"
+
+cp -R "$JENKINS_HOME/jobs" "$LOCAL_DIR"
