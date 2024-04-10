741dcb01fa304df6c0ad390826570a7fbfa80ab6
balumahendra3311 <114668682+balumahendra3311@users.noreply.github.com>
2024-04-10 21:26:05 +0530

    Update jenkins_backup.sh

--- jenkins_backup.sh
+++ jenkins_backup.sh
@@ -5,4 +5,4 @@ GIT_REPO="https://github.com/balumahendra3311/jenkinsbackup.git"
 LOCAL_DIR="/home/ubuntu/jenkins/backup/jenkinsbackup"
 JENKINS_HOME="/var/lib/jenkins"
 
-cp -R "$JENKINS_HOME/jobs" "$LOCAL_DIR"
+sudo cp -R "$JENKINS_HOME/jobs" "$LOCAL_DIR"
